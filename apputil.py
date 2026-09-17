b8c7c35a33d640d9b3fcd9e9ea37bc266a6bacce
Parmindar Singh <parmindarmundra@gmail.com>
2026-09-16 21:57:19 -0400

    adding all updated files

--- apputil.py
+++ apputil.py
@@ -3,11 +3,86 @@ import numpy as np
 
 # update/add code below ...
 
+# defining function ways
 def ways(n):
-    return None
+    """_summary_
 
+    Args:
+        n (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # start off with counters set at 0 for ways to make change
+    # also start counter for nickels at 0
+    ways_count = 0
+    nickels = 0
+    # while the amount of nickels is less than or equal to the amount of change we want to make
+    while nickels <= n:
+        # add 1 to the ways count for each way to make change
+        ways_count += 1
+        # also add 5 to the nickels counter for each way to make change
+        nickels += 5
+    # return the ways count
+    return ways_count
+
+import numpy as np
+
+
+# update/add code below ...
+
+# defining function ways
+def ways(n):
+    """_summary_
+
+    Args:
+        n (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # start off with counters set at 0 for ways to make change
+    # also start counter for nickels at 0
+    ways_count = 0
+    nickels = 0
+    # while the amount of nickels is less than or equal to the amount of change we want to make
+    while nickels <= n:
+        # add 1 to the ways count for each way to make change
+        ways_count += 1
+        # also add 5 to the nickels counter for each way to make change
+        nickels += 5
+    # return the ways count
+    return ways_count
+
+
+# defining function lowest_score
 def lowest_score(names, scores):
-    return None
+    """_summary_
 
+    Args:
+        names (_type_): _description_
+        scores (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # find the index of the lowest score in the scores array
+    lowest_score_index = np.argmin(scores)
+    # use the lowest score index to find and return the student name
+    return names[lowest_score_index]
+
+# defining function sort_names
 def sort_names(names, scores):
-    return None
+    """_summary_
+
+    Args:
+        names (_type_): _description_
+        scores (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # sort the scores in descending order and return the names in that order
+    sorted_names = np.argsort(scores)[::-1]
+    # return the names in the order of the sorted scores
+    return names[sorted_names]
